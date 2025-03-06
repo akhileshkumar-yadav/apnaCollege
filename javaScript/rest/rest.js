29afd9ed0cfca4974bff0146e39796d1e2e153ef
Akhilesh Kumar <akhileshyadav90863@gmail.com>
2025-03-06 11:31:14 +0530

    dom method

--- javaScript/rest/rest.js
+++ javaScript/rest/rest.js
@@ -9,11 +9,11 @@ let fun = (...argu ) => {
 let result = fun(1,3,5,7,9);
 
 
-let fun2 = () => {
-    console.log(arguments);
-}
+// let fun2 = () => {
+//     return arguments
+// }
 
 
-let result2 = fun2(2,4,6,8);
-console.log(result2);
+// let result2 = fun2(2,4,6,8);
+// console.log(result2);
 
